778f4566b3b39f89e63222465acd3bd778546033
aycelacap <raycel5lacap@gmail.com>
2024-01-25 13:31:23 -0500

    Add attr_reader and volume to 3d shapes

--- 3d_shapes/cube.rb
+++ 3d_shapes/cube.rb
@@ -1,6 +1,12 @@
 class Cube 
 
+  attr_reader :side_length
+
   def initialize(side_length)
     @side_length = side_length
+  end
+
+  def volume 
+    side_length * side_length * side_length
   end
 end
--- 3d_shapes/sphere.rb
+++ 3d_shapes/sphere.rb
@@ -1,7 +1,13 @@
 class Sphere 
 
+  attr_reader :radius
+
   def initialize(radius)
     @radius = radius
+  end
+
+  def volume 
+    (4/3) * radius * radius * radius * Math::PI  
   end
 
 end
