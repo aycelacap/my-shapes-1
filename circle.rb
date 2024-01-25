837f98a5807cf044c748a0780e3c85005c4847a4
aycelacap <raycel5lacap@gmail.com>
2024-01-25 13:51:04 -0500

    Add #area to Circle

--- circle.rb
+++ circle.rb
@@ -1,9 +1,15 @@
 class Circle 
 
+  attr_reader :radius
+
   def initialize(radius, x, y)
     @radius = radius
     @x = x
     @y = y
+  end
+
+  def area 
+    Math::PI * @radius ** 2
   end
 
 end
